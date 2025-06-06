9cc96c3afbed54b564dd74f088364b76b8132f09
Ucaki <uros.andjelkovic1997@gmail.com>
2025-06-06 09:28:38 -0400

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+PSQL="psql --username=freecodecamp -d guessing_game -t --no-align -c"
+
+NUMBER=$(( RANDOM % 400  + 1 ))
+echo Enter your username:
+read USER
+
+# user exist in db
+#echo Welcome back, $USER! You have played <games_played> games, and your best game took <best_game> guesses.
+
+# user dont exist
+echo Welcome, $USER! It looks like this is your first time here.
+#probbly enter him in base here?
+
+echo Guess the secret number between 1 and 1000:
+GUESS=0
+COUNTER=0
+
+while (( $GUESS != $NUMBER ))
+  do
+    read GUESS
+    if [[ $GUESS =~ ^[0-9]+$ ]]
+      then
+        (( COUNTER ++ ))
+
+        if (( $GUESS < $NUMBER ))
+        then
+          echo "It's higher than that, guess again:"
+          
+          continue
+        fi
+
+        if  (( $GUESS > $NUMBER ))
+        then
+          echo "It's lower than that, guess again:"
+          
+          continue
+        fi
+
+    else
+    echo That is not an integer, guess again:
+    
+    fi
+  done
+
+
+  echo You guessed it in $COUNTER tries. The secret number was $NUMBER. Nice job!
+
+
